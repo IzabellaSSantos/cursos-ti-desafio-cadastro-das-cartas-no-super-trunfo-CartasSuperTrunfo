716a5ac0117d6edd7dad664723ff5aa1d1097215
Izabella <izabellasoad@gmail.com>
2025-03-11 11:03:31 -0400

    Inclusão Densidade populacional e PIB per capita

--- CartasSuperTrunfoIzabella/super_trunfo.c
+++ CartasSuperTrunfoIzabella/super_trunfo.c
@@ -5,15 +5,15 @@ int main() {
     printf("Carta 01\n");
 
     int numerodePontosTuristicos;
-    float PIB, area;
+    float PIB, area, divisaoDensidade, divisaoPIB;
     double populacao;
-    char nomeEstado[50], nomeCidade[50], codigoCidade[4];
+    char estado[50], cidade[50], codigoCidade[4];
 
-    printf("Qual é o nome do estado?: \n");
-    scanf("%s", nomeEstado);
+    printf("Qual é o estado?: \n");
+    scanf(" %s", estado);
 
-    printf("Qual é o nome da cidade?: \n");
-    scanf(" %s", nomeCidade);
+    printf("Qual é a cidade?: \n");
+    scanf(" %s", cidade);
 
     printf("Código da cidade: \n");
     scanf(" %s", codigoCidade);
@@ -21,28 +21,34 @@ int main() {
     printf("Digite o PIB: \n");
     scanf(" %f", &PIB);
 
-    printf("Qual é a área total da cidade?: \n");
-    scanf("%f", &area);
+    printf("Área total da cidade?: \n");
+    scanf(" %f", &area);
 
-    printf("Qual a estimativa total da população?: \n");
+    printf("Estimativa total da população?: \n");
     scanf("%lf", &populacao);
 
     printf("Há quantos pontos turísticos na cidade?: \n");
     scanf("%d", &numerodePontosTuristicos);
 
+    divisaoDensidade = populacao / area;
+    printf("Densidade Populacional: %f\n", divisaoDensidade);
+
+    divisaoPIB = PIB / populacao;
+    printf("PIB per Capita: %f\n", divisaoPIB);
+
     printf("\n");
     printf("Carta 02\n");
 
     int numerodePontosTuristicos2;
-    float PIB2, area2;
+    float PIB2, area2, divisaoDensidade2, divisaoPIB2;
     double populacao2;
-    char nomeEstado2[50], nomeCidade2[50], codigoCidade2[4];
+    char estado2[50], cidade2[50], codigoCidade2[4];
 
-    printf("Qual é o nome do estado?: \n");
-    scanf("%s", nomeEstado2);
+    printf("Qual é o estado?: \n");
+    scanf(" %s", estado2);
 
-    printf("Qual é o nome da cidade?: \n");
-    scanf(" %s", nomeCidade2);
+    printf("Qual é a cidade?: \n");
+    scanf(" %s", cidade2);
 
     printf("Código da cidade: \n");
     scanf(" %s", codigoCidade2);
@@ -50,32 +56,46 @@ int main() {
     printf("Digite o PIB: \n");
     scanf(" %f", &PIB2);
 
-    printf("Qual é a área total da cidade?: \n");
+    printf("Área total da cidade?: \n");
     scanf("%f", &area2);
 
-    printf("Qual a estimativa total da população?: \n");
+    printf("Estimativa total da população?: \n");
     scanf("%lf", &populacao2);
 
     printf("Há quantos pontos turísticos na cidade?: \n");
     scanf("%d", &numerodePontosTuristicos2);
 
+    divisaoDensidade2 = populacao2 / area2;
+    printf("Densidade Populacional: %f\n", divisaoDensidade2);
+
+    divisaoPIB2 = PIB2 / populacao2;
+    printf("PIB per Capita: %f\n\n", divisaoPIB2);
+
+    
     printf("----- Carta 01 -----\n");
-    printf("Estado: %s\n", nomeEstado);
-    printf("Cidade: %s\n", nomeCidade);
+    printf("Estado: %s\n", estado);
+    printf("Cidade: %s\n", cidade);
     printf("Código da Cidade: %s\n", codigoCidade);
     printf("PIB: %f\n", PIB);
     printf("Área: %f\n", area);
     printf("População: %lf\n", populacao);
-    printf("Pontos turísticos: %d\n\n", numerodePontosTuristicos);
+    printf("Pontos turísticos: %d\n", numerodePontosTuristicos);
+    printf("Densidade Populacional: %f\n", divisaoDensidade);
+    printf("PIB per Capita: %f\n", divisaoPIB2);
 
     printf("----- Carta 02 -----\n");
-    printf("Estado: %s\n", nomeEstado2);
-    printf("Cidade: %s\n", nomeCidade2);
+    printf("Estado: %s\n", estado2);
+    printf("Cidade: %s\n", cidade2);
     printf("Código da Cidade: %s\n", codigoCidade2);
     printf("PIB: %f\n", PIB2);
     printf("Área: %f\n", area2);
     printf("População: %lf\n", populacao2);
-    printf("Pontos turísticos: %d\n\n", numerodePontosTuristicos2);
+    printf("Pontos turísticos: %d\n", numerodePontosTuristicos2);
+    printf("Densidade Populacional: %f\n", divisaoDensidade2);
+    printf("PIB per Capita: %f\n", divisaoPIB2);
+
+
+
 
     return 0;
 }
